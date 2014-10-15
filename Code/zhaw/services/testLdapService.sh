307e149accd75a38d343867ea4296a27dc28848d
Roman Muentener <munt@zhaw.ch>
2014-10-15 14:35:10 +0200

    * change URL

--- Code/zhaw/services/testLdapService.sh
+++ Code/zhaw/services/testLdapService.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-curl -X GET 'http://localhost:8888/ldap-ui-service/schemaDump?oc=abcPerson' > in.xml
-curl -X POST --header 'Content-Type: application/xml' -d @in.xml 'http://localhost:8888/ldap-ui-service/genCredSpec' > credSpec.xml
-curl -X POST --header 'Content-Type: application/xml' -d @credSpec.xml 'http://localhost:8888/ldap-ui-service/genIssuanceAttributes' > outAttribs.xml
+curl -X GET 'http://localhost:8888/zhaw-p2abc-webservices/schemaDump?oc=abcPerson' > in.xml
+curl -X POST --header 'Content-Type: application/xml' -d @in.xml 'http://localhost:8888/zhaw-p2abc-webservices/genCredSpec' > credSpec.xml
+curl -X POST --header 'Content-Type: application/xml' -d @credSpec.xml 'http://localhost:8888/zhaw-p2abc-webservices/genIssuanceAttributes' > outAttribs.xml
