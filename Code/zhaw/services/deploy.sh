f83ddd41a485a88d2a5b355a4d2f81d53a754c48
Roman Muentener <munt@zhaw.ch>
2014-10-16 13:40:17 +0200

    + need sudo there

--- Code/zhaw/services/deploy.sh
+++ Code/zhaw/services/deploy.sh
@@ -8,5 +8,5 @@ rm -f ~/web/web1/webapps/*.war
 cp -f ./target/zhaw-p2abc-webservices.war ~/web/web1/webapps/zhaw-p2abc-webservices.war
 cp -rf ./target/zhaw-p2abc-webservices ~/web/web1/webapps/
 cp -f ./src-web/ldapui.html ~/web/web1/webapps/ROOT/ldapui.html
-cp -f ldapServiceConfig.xml /etc/abc4trust/ldapServiceConfig.xml
+sudo cp -f ldapServiceConfig.xml /etc/abc4trust/ldapServiceConfig.xml
 ~/web/web1/bin/startup.sh
