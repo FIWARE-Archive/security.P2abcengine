32960e7eb0c43d3a044b60e2ee80bbf70c7a3a97
Roman Muentener <munt@zhaw.ch>
2014-10-22 10:47:06 +0200

    + started putNew

--- Code/zhaw/components/src/main/java/ch/zhaw/ficore/p2abc/storage/SqliteURIBytesStorage.java
+++ Code/zhaw/components/src/main/java/ch/zhaw/ficore/p2abc/storage/SqliteURIBytesStorage.java
@@ -6,6 +6,7 @@ import java.sql.*;
 
 public class SqliteURIBytesStorage {
 	private Connection con;
+	private String table;
 	
 	public SqliteURIBytesStorage(String filePath, String table) {
 		try {
@@ -19,10 +20,30 @@ public class SqliteURIBytesStorage {
 		                   " value         BLOB     NOT NULL)";
 		    stmt.executeUpdate(sql);
 		    stmt.close();
+		    this.table = table;
 		}
 		catch(Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException("Failed to open storage!");
+		}
+	}
+	
+	public void putNew(URI uri, byte[] bytes) {
+		try {
+			PreparedStatement pStmt = con.prepareStatement("INSERT INTO " + table + "(hash, uri, value) " +
+							"VALUES(?, ?, ?)");
+			
+			String hash = "foo"; //TODO: Calc hash here :D
+			
+			pStmt.setString(1, hash);
+			pStmt.setString(2, uri.toString());
+			pStmt.setBytes(3, bytes);
+			
+			pStmt.executeUpdate();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("Storage failure!");
 		}
 	}
 }
