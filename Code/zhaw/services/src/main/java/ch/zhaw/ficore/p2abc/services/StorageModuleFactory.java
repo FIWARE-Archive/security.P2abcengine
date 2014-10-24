f730f05def6c818db64d38bae850ff6dc8c2f4e3
Roman Muentener <munt@zhaw.ch>
2014-10-24 14:56:30 +0200

    + some javadoc

--- Code/zhaw/services/src/main/java/ch/zhaw/ficore/p2abc/services/StorageModuleFactory.java
+++ Code/zhaw/services/src/main/java/ch/zhaw/ficore/p2abc/services/StorageModuleFactory.java
@@ -10,10 +10,21 @@ import ch.zhaw.ficore.p2abc.services.guice.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * Serves as a Factory for StorageModules where a StorageModule
+ * is an AbstractModule from Guice with bindings.
+ * 
+ * @author mroman
+ */
 public class StorageModuleFactory {
 	
 	private static Logger logger = LogManager.getLogger(StorageModuleFactory.class.getName());
 	
+	/**
+	 * Factory method. Returns an array of Modules.
+	 * 
+	 * @return Array of Modules that shall be used to overwrite existing guice bindings.
+	 */
 	public static synchronized Module[] getModulesForServiceConfiguration(ServicesConfiguration config, ServicesConfiguration.ServiceType type) {
 		logger.entry();
 		
