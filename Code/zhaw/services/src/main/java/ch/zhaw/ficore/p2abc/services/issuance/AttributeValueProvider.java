af4be7ad96b9a47c4b3900b45831f0ad8ca20a00
Roman Muentener <munt@zhaw.ch>
2014-10-29 14:30:58 +0100

    + indent

--- Code/zhaw/services/src/main/java/ch/zhaw/ficore/p2abc/services/issuance/AttributeValueProvider.java
+++ Code/zhaw/services/src/main/java/ch/zhaw/ficore/p2abc/services/issuance/AttributeValueProvider.java
@@ -15,56 +15,56 @@ import java.util.List;
  * @author mroman
  */
 public abstract class AttributeValueProvider {
-	
-	protected IssuanceConfigurationData configuration;
-	
-	/**
-	 * Constructor of an AttributeValueProvider.
-	 * 
-	 * The query parameter tells the AttributeValueProvider how to fetch the
-	 * values. E.g., for LDAP query might be an LDAP-Search query. 
-	 * 
-	 * @param configuration Configuration.
-	 */
-	public AttributeValueProvider(IssuanceConfigurationData configuration) {
-		this.configuration = configuration;
-	}
-	
-	/**
-	 * Factory method to construct an AttributeValueProvider for a given
-	 * ServiceConfiguration. The AttributeValueProvider will receive
-	 * a reference to the configuration.
-	 * 
-	 * @param configuration Issuance configuration
-	 * @return an implementation of an AttributeValueProvider
-	 */
-	public static AttributeValueProvider getAttributeValueProvider(IssuanceConfigurationData configuration) {
-		switch(configuration.getAttributeSource()) {
-		case LDAP:
-			return new LdapAttributeValueProvider(configuration);
-		default:
-	    return null;
-		}
-	}
-	
-	/**
-	 * Called when this AttributeValueProvider is no longer required. 
-	 * Providers should close open sockets/connections/files etc. on shutdown.
-	 */
-	public abstract void shutdown();
-	
-	
-	
-	/**
-	 * Obtains the attributes to be embedded in the IssuancePolicyAndAttributes. 
-	 * The query parameter tells the AttributeValueProvider how to fetch the values.
-	 * E.g. for LDAP query might be an LDAP-Search query. The exact behaviour of <em>query</em> is provider
-	 * specific. 
-	 * 
-	 * @param query Query (see description above) 
-	 * @param uid UserID
-	 * @param credSpec CredentialSpecification to obtain attributes for. 
-	 */
-	public abstract List<eu.abc4trust.xml.Attribute> getAttributes(String query, String uid,
-			CredentialSpecification credSpec) throws Exception;
+
+    protected IssuanceConfigurationData configuration;
+
+    /**
+     * Constructor of an AttributeValueProvider.
+     * 
+     * The query parameter tells the AttributeValueProvider how to fetch the
+     * values. E.g., for LDAP query might be an LDAP-Search query. 
+     * 
+     * @param configuration Configuration.
+     */
+    public AttributeValueProvider(IssuanceConfigurationData configuration) {
+        this.configuration = configuration;
+    }
+
+    /**
+     * Factory method to construct an AttributeValueProvider for a given
+     * ServiceConfiguration. The AttributeValueProvider will receive
+     * a reference to the configuration.
+     * 
+     * @param configuration Issuance configuration
+     * @return an implementation of an AttributeValueProvider
+     */
+    public static AttributeValueProvider getAttributeValueProvider(IssuanceConfigurationData configuration) {
+        switch(configuration.getAttributeSource()) {
+        case LDAP:
+            return new LdapAttributeValueProvider(configuration);
+        default:
+            return null;
+        }
+    }
+
+    /**
+     * Called when this AttributeValueProvider is no longer required. 
+     * Providers should close open sockets/connections/files etc. on shutdown.
+     */
+    public abstract void shutdown();
+
+
+
+    /**
+     * Obtains the attributes to be embedded in the IssuancePolicyAndAttributes. 
+     * The query parameter tells the AttributeValueProvider how to fetch the values.
+     * E.g. for LDAP query might be an LDAP-Search query. The exact behaviour of <em>query</em> is provider
+     * specific. 
+     * 
+     * @param query Query (see description above) 
+     * @param uid UserID
+     * @param credSpec CredentialSpecification to obtain attributes for. 
+     */
+    public abstract List<eu.abc4trust.xml.Attribute> getAttributes(String query, String uid,
+            CredentialSpecification credSpec) throws Exception;
 }
--- Code/zhaw/services/src/main/java/ch/zhaw/ficore/p2abc/services/issuance/LdapAuthenticationProvider.java
+++ Code/zhaw/services/src/main/java/ch/zhaw/ficore/p2abc/services/issuance/LdapAuthenticationProvider.java
@@ -19,118 +19,118 @@ import javax.naming.directory.*;
  * @author mroman
  */
 public class LdapAuthenticationProvider extends AuthenticationProvider {
-	
-	private Logger logger;
-	private boolean authenticated = false;
-	private String uid;
-	
-	/**
-	 * Constructor
-	 * @throws NamingException 
-	 */
-	public LdapAuthenticationProvider(IssuanceConfigurationData configuration)  {
-		super(configuration);
-		logger = LogManager.getLogger();
-	}
-	
-	/**
-	 * No operation.
-	 */
-	public void shutdown() {
-		
-	}
-	
-	/**
-	 * Performs the authentication through Ldap.
-	 * 
-	 * @return true if successful, false otherwise.
-	 */
-	public boolean authenticate(AuthenticationInformation authInfo) {
-	  boolean isGood = true;
 
-	  logger.entry();
+  private Logger logger;
+  private boolean authenticated = false;
+  private String uid;
 
-	  if(!(authInfo instanceof AuthInfoSimple)) {
-	    logger.warn("LDAP AuthenticationInformation is not simple, can't use username/password");
-	    isGood = false;
-	  }
+  /**
+   * Constructor
+   * @throws NamingException 
+   */
+  public LdapAuthenticationProvider(IssuanceConfigurationData configuration)  {
+    super(configuration);
+    logger = LogManager.getLogger();
+  }
 
-	  if (isGood) {
-	    AuthInfoSimple simpleAuth = (AuthInfoSimple) authInfo;
-	    IssuanceConfigurationData configuration = ServicesConfiguration.getIssuanceConfiguration();
-	    if(configuration.getAuthenticationConnectionParameters().usesTls())
-	      throw logger.throwing(new RuntimeException("TLS not supported yet :("));
+  /**
+   * No operation.
+   */
+  public void shutdown() {
 
-	    LdapConnection adminConnection = null;
-	    LdapConnection userConnection = null;
+  }
 
-	    try {
-	      String bindQuery = QueryHelper.buildQuery(configuration.getBindQuery(), 
-	          QueryHelper.ldapSanitize(simpleAuth.username));
-	      System.out.println("q:"+bindQuery);
+  /**
+   * Performs the authentication through Ldap.
+   * 
+   * @return true if successful, false otherwise.
+   */
+  public boolean authenticate(AuthenticationInformation authInfo) {
+    boolean isGood = true;
 
-	      ConnectionParameters adminCfg = configuration.getAuthenticationConnectionParameters();
-	      adminConnection = new LdapConnection(adminCfg);
+    logger.entry();
 
-	      NamingEnumeration<SearchResult> results = adminConnection.newSearch().search("", bindQuery);
-	      String binddn = null;
-	      if (results.hasMore()) {
-	        SearchResult sr = (SearchResult) results.next();
-	        binddn = sr.getName();
-	      }
-	      System.out.println(binddn);
+    if(!(authInfo instanceof AuthInfoSimple)) {
+      logger.warn("LDAP AuthenticationInformation is not simple, can't use username/password");
+      isGood = false;
+    }
 
-	      if (binddn == null) {
-	        logger.warn ("Couldn't find DN for user " + simpleAuth.username);
-	        isGood = false;
-	      }
+    if (isGood) {
+      AuthInfoSimple simpleAuth = (AuthInfoSimple) authInfo;
+      IssuanceConfigurationData configuration = ServicesConfiguration.getIssuanceConfiguration();
+      if(configuration.getAuthenticationConnectionParameters().usesTls())
+        throw logger.throwing(new RuntimeException("TLS not supported yet :("));
 
-	      if (isGood) {
-	        ConnectionParameters userCfg = new ConnectionParameters(adminCfg.getServerName(), adminCfg.getServerPort(), adminCfg.getServerPort(), adminCfg.getServerPort(), binddn, simpleAuth.password.toCharArray(), adminCfg.usesTls());
-	        // Implicit authentication
-	        userConnection = new LdapConnection(userCfg);
+      LdapConnection adminConnection = null;
+      LdapConnection userConnection = null;
 
-	        // If no exception thrown above, then user is authenticated
-	        authenticated = true;
-	        uid = simpleAuth.username;
+      try {
+        String bindQuery = QueryHelper.buildQuery(configuration.getBindQuery(), 
+            QueryHelper.ldapSanitize(simpleAuth.username));
+        System.out.println("q:"+bindQuery);
 
-	        userConnection.close();
-	        adminConnection.close();
-	      }
-	    }
-	    catch(Exception e) {
-	      logger.catching(e);
-	      return logger.exit(false);
-	    }
-	    finally {
-	      try {
-	        if (adminConnection != null)
-	          adminConnection.close();
-	      }
-	      catch(Exception e) {
-	        logger.catching(e);
-	        isGood = false;
-	      }      
-	      try {
-	        if (userConnection != null)
-	          userConnection.close();
-	      }
-	      catch(Exception e) {
-	        logger.catching(e);
-	        isGood = false;
-	      }
-	    }
-	  }
-	  
-	  /* N.B.: !isGood && authenticated is possible, so we must return
-	   * authenticated here, not isGood. */
-	  return logger.exit(authenticated);
-	}
-	
-	public String getUserID() {
-		if(!authenticated)
-			throw new IllegalStateException("Must successfully authenticate prior to calling this method!");
-		
-		return uid;
-	}
+        ConnectionParameters adminCfg = configuration.getAuthenticationConnectionParameters();
+        adminConnection = new LdapConnection(adminCfg);
+
+        NamingEnumeration<SearchResult> results = adminConnection.newSearch().search("", bindQuery);
+        String binddn = null;
+        if (results.hasMore()) {
+          SearchResult sr = (SearchResult) results.next();
+          binddn = sr.getName();
+        }
+        System.out.println(binddn);
+
+        if (binddn == null) {
+          logger.warn ("Couldn't find DN for user " + simpleAuth.username);
+          isGood = false;
+        }
+
+        if (isGood) {
+          ConnectionParameters userCfg = new ConnectionParameters(adminCfg.getServerName(), adminCfg.getServerPort(), adminCfg.getServerPort(), adminCfg.getServerPort(), binddn, simpleAuth.password.toCharArray(), adminCfg.usesTls());
+          // Implicit authentication
+          userConnection = new LdapConnection(userCfg);
+
+          // If no exception thrown above, then user is authenticated
+          authenticated = true;
+          uid = simpleAuth.username;
+
+          userConnection.close();
+          adminConnection.close();
+        }
+      }
+      catch(Exception e) {
+        logger.catching(e);
+        return logger.exit(false);
+      }
+      finally {
+        try {
+          if (adminConnection != null)
+            adminConnection.close();
+        }
+        catch(Exception e) {
+          logger.catching(e);
+          isGood = false;
+        }      
+        try {
+          if (userConnection != null)
+            userConnection.close();
+        }
+        catch(Exception e) {
+          logger.catching(e);
+          isGood = false;
+        }
+      }
+    }
+
+    /* N.B.: !isGood && authenticated is possible, so we must return
+     * authenticated here, not isGood. */
+    return logger.exit(authenticated);
+  }
+
+  public String getUserID() {
+    if(!authenticated)
+      throw new IllegalStateException("Must successfully authenticate prior to calling this method!");
+
+    return uid;
+  }
 }
