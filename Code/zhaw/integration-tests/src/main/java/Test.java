165db4a374767c19603427372d92944d1e69a1fa
Roman Muentener <munt@zhaw.ch>
2014-11-06 15:54:54 +0100

    + testAuthentication

--- Code/zhaw/integration-tests/src/main/java/Test.java
+++ Code/zhaw/integration-tests/src/main/java/Test.java
@@ -3,6 +3,7 @@ import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
 import static org.junit.Assert.*;
+import java.io.*;
 
 public class Test {
 
@@ -24,11 +25,45 @@ public class Test {
         
         /* Ok, if we are here all services are at least running */
         
+        /* Test authentication */
+        testAuthentication(readTextFile("simpleAuth.xml"));
+        
         /* Get an attributeInfoCollection and convert it to a credentialSpecification */
         String attributeInfoCollection = testAttributeInfoCollection();
         String credSpec = testGenCredSpec(attributeInfoCollection);
 
         System.out.println("I'm done!");
+    }
+    
+    public static String readTextFile(String path) {
+        try {
+            ClassLoader cl = Test.class.getClassLoader();
+            File f = new File(cl.getResource(path).getFile());
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            String lines = "";
+            String line = "";
+            while((line = br.readLine()) != null)
+                lines += line + "\n";
+            br.close();
+            return lines;
+        }
+        catch(Exception e) {
+            throw new RuntimeException("readTextFile("+path+") failed!");
+        }
+    }
+    
+    public static String testAuthentication(String authRequest) {
+        Client client = Client.create();
+
+        WebResource webResource = client
+                .resource(issuanceServiceURL + "testAuthentication");
+
+        ClientResponse response = webResource.type("application/xml")
+                .post(ClientResponse.class, authRequest);
+
+        assertTrue(response.getStatus() == 200);
+        
+        return response.getEntity(String.class);
     }
     
     public static String testGenCredSpec(String attributeInfoCollection) {
@@ -78,9 +113,7 @@ public class Test {
 
         ClientResponse response = webResource.get(ClientResponse.class);
 
-        assertTrue(response.getStatus() == 200);
-        
-        
+        assertTrue(response.getStatus() == 200);       
     }
     
     public static void testVerificationStatus() {
@@ -91,9 +124,7 @@ public class Test {
 
         ClientResponse response = webResource.get(ClientResponse.class);
 
-        assertTrue(response.getStatus() == 200);
-        
-        
+        assertTrue(response.getStatus() == 200); 
     }
     
 }
