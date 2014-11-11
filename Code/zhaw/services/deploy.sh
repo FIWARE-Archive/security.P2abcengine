f9305acaba8189cb50752d0075bf0411cd1e25ef
Roman Muentener <munt@zhaw.ch>
2014-11-11 16:54:58 +0100

    * fixing tomcat () 404 issues

--- Code/zhaw/services/deploy.sh
+++ Code/zhaw/services/deploy.sh
@@ -7,10 +7,10 @@ sudo /etc/init.d/apacheds-2.0.0-M17-default start
 
 echo "Building components"
 cd ../components
-mvn clean install -DskipTests
+mvn clean install
 cd ../services
 
-echo "Building Services"
+echo "Building Services (profile)"
 mvn clean
 mvn -P ldap-ui-service install
 ~/web/web1/bin/shutdown.sh
@@ -18,6 +18,4 @@ rm -rf ~/web/web1/webapps/zhaw-p2abc-webservices
 rm -f ~/web/web1/webapps/*.war
 cp -f ./target/zhaw-p2abc-webservices.war ~/web/web1/webapps/zhaw-p2abc-webservices.war
 #cp -rf ./target/zhaw-p2abc-webservices ~/web/web1/webapps/
-cp -f ./src-web/ldapui.html ~/web/web1/webapps/ROOT/ldapui.html
-sudo cp -f ldapServiceConfig.xml /etc/abc4trust/ldapServiceConfig.xml
 ~/web/web1/bin/startup.sh
