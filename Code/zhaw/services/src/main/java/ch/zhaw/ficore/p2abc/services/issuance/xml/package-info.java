08e53887695a7a5887008052eb558c3529865eac
Stephan Neuhaus <stephan.neuhaus@zhaw.ch>
2014-11-21 13:40:21 +0100

    Still trying to understand schemagen

--- Code/zhaw/services/src/main/java/ch/zhaw/ficore/p2abc/services/issuance/xml/package-info.java
+++ Code/zhaw/services/src/main/java/ch/zhaw/ficore/p2abc/services/issuance/xml/package-info.java
@@ -1,8 +1,8 @@
 @XmlSchema(
-xmlns = {
-@XmlNs(prefix = "abc", namespaceURI ="http://abc4trust.eu/wp2/abcschemav1.0"),
-},
-elementFormDefault = XmlNsForm.QUALIFIED
+        xmlns = {
+                @XmlNs(prefix = "abc", namespaceURI ="http://abc4trust.eu/wp2/abcschemav1.0"),
+        },
+        elementFormDefault = XmlNsForm.QUALIFIED
 ) 
 package ch.zhaw.ficore.p2abc.services.issuance.xml;
 import javax.xml.bind.annotation.XmlSchema;
