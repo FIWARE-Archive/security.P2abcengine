1ea18575d65e3399bbdad18566cc5e7f34a62b02
Roman Muentener <munt@zhaw.ch>
2014-11-21 16:43:42 +0100

    * organize imports

--- Code/zhaw/services/src/main/java/ch/zhaw/ficore/p2abc/services/issuance/xml/Settings.java
+++ Code/zhaw/services/src/main/java/ch/zhaw/ficore/p2abc/services/issuance/xml/Settings.java
@@ -3,10 +3,8 @@ package ch.zhaw.ficore.p2abc.services.issuance.xml;
 import java.util.List;
 
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementRef;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlSeeAlso;
 
 import eu.abc4trust.xml.CredentialSpecification;
 import eu.abc4trust.xml.IssuerParameters;
