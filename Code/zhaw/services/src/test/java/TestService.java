a49e725e8ed26d6b9f0fc7c425d0e24e0f410bc3
Roman Muentener <munt@zhaw.ch>
2014-11-27 15:21:43 +0100

    + finally actually run the REST-API through a embedded web-server

--- Code/zhaw/services/src/test/java/TestService.java
+++ Code/zhaw/services/src/test/java/TestService.java
@@ -1,4 +1,9 @@
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -7,27 +12,38 @@ import javax.ws.rs.core.Response;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import org.mortbay.jetty.Server;
 import org.sqlite.SQLiteDataSource;
 
 import ch.zhaw.ficore.p2abc.configuration.ConnectionParameters;
 import ch.zhaw.ficore.p2abc.services.helpers.RESTHelper;
 import ch.zhaw.ficore.p2abc.services.user.UserService;
 
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import com.sun.jersey.api.core.PackagesResourceConfig;
+import com.sun.jersey.api.core.ResourceConfig;
+import com.sun.jersey.api.core.ScanningResourceConfig;
+import com.sun.jersey.test.framework.JerseyTest;
+import com.sun.jersey.test.framework.TestConstants;
+import com.sun.net.httpserver.HttpServer;
 
-public class TestService {
+
+public class TestService extends JerseyTest {
+
+    public TestService() throws Exception {
+        super("ch.zhaw.ficore.p2abc.services");
+        initJNDI();
+    }
 
     UserService userService;
 
     private static String getBaseURI() {
-        return "http://localhost:8989/zhaw-p2abc-webservices/";
+        return "http://localhost:" + TestConstants.JERSEY_HTTP_PORT + "/";
     }
     
     File storageFile;
     String dbName = "URIBytesStorage";
     
-    public void launchServer() throws Exception {
+    public void initJNDI() throws Exception {
      // Create initial context
         System.setProperty(Context.INITIAL_CONTEXT_FACTORY,
                 "org.apache.naming.java.javaURLContextFactory");
@@ -44,12 +60,6 @@ public class TestService {
 
         SQLiteDataSource ds = new SQLiteDataSource();
         
-        storageFile = File.createTempFile("test", "sql");
-        
-        ds.setUrl("jdbc:sqlite:" + storageFile.getPath());
-        ic.bind("java:/comp/env/jdbc/" + dbName, ds);
-        ic.bind("java:/comp/env/cfg/useDbLocking", new Boolean(true));
-        
         ConnectionParameters cp = new ConnectionParameters();
         ic.bind("java:/comp/env/cfg/ConnectionParameters/attributes", cp);
         ic.bind("java:/comp/env/cfg/ConnectionParameters/authentication", cp);
@@ -61,26 +71,41 @@ public class TestService {
         ic.bind("java:/comp/env/cfg/restAuthUser", "");
         ic.bind("java:/comp/env/cfg/issuanceServiceURL","");
         ic.bind("java:/comp/env/cfg/userServiceURL","");
-        
-        userService = new UserService();
     }
-
+    
+    
+    private static HttpServer createHttpServer() throws IOException, IllegalArgumentException, URISyntaxException {
+        ResourceConfig rc = new PackagesResourceConfig("ch.zhaw.ficore.p2abc.services");
+        return HttpServerFactory.create(new URI("http://localhost:8989/"), rc);
+    }
+    
     @Before
-    public void startServer() throws Exception {
-        System.out.println("Starting server...");
-
-        launchServer();
+    public void makeStorageFile() throws Exception {
+        SQLiteDataSource ds = new SQLiteDataSource();
+        
+        InitialContext ic = new InitialContext();
+        
+        storageFile = File.createTempFile("test", "sql");
+        
+        ds.setUrl("jdbc:sqlite:" + storageFile.getPath());
+        ic.bind("java:/comp/env/jdbc/" + dbName, ds);
+        ic.bind("java:/comp/env/cfg/useDbLocking", new Boolean(true));
     }
 
     @After
-    public void stopServer() throws Exception {
+    public void deleteStorageFile() throws Exception {
         storageFile.delete();
     }
 
     @Test
     public void testUserServiceStatus() throws Exception {
-        Response r = userService.status();
-        assertOk(r);
+        //Response r = userService.status();
+        //assertOk(r);
+        
+        //Thread.sleep(60000);
+        
+        RESTHelper.getRequest(getBaseURI() + "user/status");
+        
     }
     
     public void assertOk(Response r) {
