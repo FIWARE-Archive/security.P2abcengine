eff821b75a1778c4f0430f14fe2d524a54c35601
Roman Muentener <munt@zhaw.ch>
2014-12-02 13:56:35 +0100

    ! fix compilation issue + verification GUI

---
+++ Code/zhaw/services/src/main/java/ch/zhaw/ficore/p2abc/services/helpers/verification/VerificationGUI.java
@@ -0,0 +1,51 @@
+package ch.zhaw.ficore.p2abc.services.helpers.verification;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.hp.gagawa.java.elements.A;
+import com.hp.gagawa.java.elements.Body;
+import com.hp.gagawa.java.elements.Div;
+import com.hp.gagawa.java.elements.H2;
+import com.hp.gagawa.java.elements.Head;
+import com.hp.gagawa.java.elements.Html;
+import com.hp.gagawa.java.elements.Link;
+import com.hp.gagawa.java.elements.P;
+import com.hp.gagawa.java.elements.Text;
+import com.hp.gagawa.java.elements.Title;
+
+public class VerificationGUI {
+
+    private static String cssURL = "/css/style.css";
+
+
+    public static Html getHtmlPramble(String title, HttpServletRequest req) {
+        Html html = new Html();
+        Head head = new Head().appendChild(new Title().appendChild(new Text(
+                title)));
+        html.appendChild(head);
+        head.appendChild(new Link().setHref(req.getContextPath() + cssURL)
+                .setRel("stylesheet").setType("text/css"));
+        return html;
+    }
+
+    public static Body getBody(Div mainDiv) {
+        Div containerDiv = new Div().setCSSClass("containerDiv");
+        Div navDiv = new Div().setCSSClass("navDiv");
+        containerDiv.appendChild(navDiv);
+        containerDiv.appendChild(mainDiv);
+        navDiv.appendChild(new A().setHref("./profile").appendChild(
+                new Text("Profile")));
+        navDiv.appendChild(new Div().setStyle("clear: both"));
+        return new Body().appendChild(containerDiv);
+    }
+
+    public static Html errorPage(String msg, HttpServletRequest req) {
+        Html html = getHtmlPramble("ERROR", req);
+        Div mainDiv = new Div().setCSSClass("mainDiv");
+        html.appendChild(getBody(mainDiv));
+        mainDiv.appendChild(new H2().appendChild(new Text("Error")));
+        mainDiv.appendChild(new P().setCSSClass("error").appendChild(
+                new Text(msg)));
+        return html;
+    }
+}
